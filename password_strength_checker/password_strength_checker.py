584c66fda0100deea31f5389d54bc21683b93b79
Alex Maszanski <39808040+Wreemde@users.noreply.github.com>
2023-04-23 16:30:02 +0400

    Added skeleton content for the password_strength_checker module

---
+++ password_strength_checker/password_strength_checker.py
@@ -0,0 +1,14 @@
+import re
+def check_password_strength(password):
+    """
+    Checks the strength of a password.
+
+    Returns:
+        A string indicating the strength of the password.
+    """
+    #TODO: add more functionality
+
+    if :
+        return ""
+    else:
+        return ""
