33e99e8f34559d123b4adb9bf0aae0d899877c7c
Alex Maszanski <39808040+Wreemde@users.noreply.github.com>
2023-04-23 16:32:53 +0400

    Added skeletom content for the test_password_strength_checker module

---
+++ tests/test_password_strength_checker.py
@@ -0,0 +1,12 @@
+import unittest
+# TODO: find more adequate name for the imported function
+from password_strength_checker.password_strength_checker import check_password_strength
+
+class TestPasswordStrengthCheck(unittest.TestCase):
+    def test_weak_password(self):
+    #TODO: add more functionality
+    def test_medium_password(self):
+    
+    def test_strong_password(self):
+
+    def test_invalid_password(self):
