434f824dae9debc29c774e29e55c4e0fcec59abe
Alex Maszanski <39808040+Wreemde@users.noreply.github.com>
2023-04-24 18:05:38 +0400

    Added password checker identifiers in the password_strength_checker

--- password_strength_checker/password_strength_checker.py
+++ password_strength_checker/password_strength_checker.py
@@ -7,8 +7,14 @@ def check_password_strength(password):
         A string indicating the strength of the password.
     """
     #TODO: add more functionality
+    lenght_error = 
+    digit_error = 
+    uppercase_error =
+    lowercase_error =
+    symbol_error = 
+    password_ok =
 
-    if :
-        return ""
+    if password_ok:
+        return "Strong password"
     else:
-        return ""
+        return "Weak password"
