ed8bfb2e285b1ef35ff3054df49d17105585536a
Alex Maszanski <39808040+Wreemde@users.noreply.github.com>
2023-04-26 12:43:11 +0400

    Provided comments for the password_strength_checker code

--- password_strength_checker.py
+++ password_strength_checker.py
@@ -6,12 +6,16 @@ def check_password_strength(password):
     Returns:
         A string indicating the strength of the password.
     """
+
     lenght_error = len(password) < 8
-    digit_error = re.search(r"\d", password) is None
-    # !!! The 'r' before the regular expression pattern is used to denote a raw string. !!!
-    uppercase_error = re.search(r"[A-Z]", password) is None
-    lowercase_error = re.search(r"[a-z]", password) is None
-    symbol_error = re.search(r"\W", password) is None
+
+    """!!! The 'r' before the regular expression pattern is used to denote a raw string. !!!"""
+    """!!! The re.search function searches for this pattern in the password string. !!!"""
+
+    digit_error = re.search(r"\d", password) is None # \d pattern matches any digit character
+    uppercase_error = re.search(r"[A-Z]", password) is None # [A-Z] pattern matches any uppercase letter
+    lowercase_error = re.search(r"[a-z]", password) is None # [a-z] pattern matches any lowercase letter
+    symbol_error = re.search(r"\W", password) is None # \W pattern matches any non-word character (symbol)
     password_ok = not (lenght_error or digit_error or uppercase_error or lowercase_error or symbol_error)
 
     if password_ok:
