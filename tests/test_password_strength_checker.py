4c8b034556f83de5147c74243c109ad38eb2506c
Alex Maszanski <39808040+Wreemde@users.noreply.github.com>
2023-04-27 18:08:07 +0400

    Code for the test module

--- tests/test_password_strength_checker.py
+++ tests/test_password_strength_checker.py
@@ -4,9 +4,17 @@ from password_strength_checker import password_strength_check
 
 class TestPasswordStrengthCheck(unittest.TestCase):
     def test_weak_password(self):
-    #TODO: add more functionality
+        result = password_strength_check('password')
+        self.assertEqual(result, 'Weak')
+
     def test_medium_password(self):
-    
+        result = password_strength_check('P@ssword123')
+        self.assertEqual(result, 'Medium')
+
     def test_strong_password(self):
+        result = password_strength_check('MyP@ssw0rd!sV3ryStr0ng') # actually not
+        self.assertEqual(result, 'Strong')
 
     def test_invalid_password(self):
+        result = password_strength_check('12345')
+        self.assertEqual(result, 'Invalid')
