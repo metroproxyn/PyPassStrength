492f84dcf52a424dfa3a5f0e6e74deb02f68b89d
Alex Maszanski <39808040+Wreemde@users.noreply.github.com>
2023-05-02 11:27:27 +0400

    Added shell script to make the programm easily executable

---
+++ run_password_strength_checker.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+python3 password_strength_checker.py $@
+
+# In a shell script, the $@ variable represents all of the positional parameters passed to the script.
+
+# Note that the shebang line is only used on Unix-like systems (including Linux and macOS); 
+# on Windows, the file extension is used to determine the interpreter to use.
